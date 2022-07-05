12931f0b965ac19b7bc1e5bbaade8d1c618aff1c
bengdaraamina <100786296+bengdaraamina@users.noreply.github.com>
2022-07-05 18:29:26 +0200

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,5 +1,5 @@
 #display the output
-print("new peython file")
+print("new python file")
 Final Assignment Statistics for Data Science
 import numpy as np
 import pandas as pd
@@ -10,19 +10,13 @@ import scipy.stats as stats
 boston_url = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ST0151EN-SkillsNetwork/labs/boston_housing.csv'
 boston_df=pd.read_csv(boston_url)
 boston_df.head()
-Unnamed: 0	CRIM	ZN	INDUS	CHAS	NOX	RM	AGE	DIS	RAD	TAX	PTRATIO	LSTAT	MEDV
-0	0	0.00632	18.0	2.31	0.0	0.538	6.575	65.2	4.0900	1.0	296.0	15.3	4.98	24.0
-1	1	0.02731	0.0	7.07	0.0	0.469	6.421	78.9	4.9671	2.0	242.0	17.8	9.14	21.6
-2	2	0.02729	0.0	7.07	0.0	0.469	7.185	61.1	4.9671	2.0	242.0	17.8	4.03	34.7
-3	3	0.03237	0.0	2.18	0.0	0.458	6.998	45.8	6.0622	3.0	222.0	18.7	2.94	33.4
-4	4	0.06905	0.0	2.18	0.0	0.458	7.147	54.2	6.0622	3.0	222.0	18.7	5.33	36.2
+
 sns.boxplot(x="CHAS", y="MEDV", data=boston_df).set_title('Median value depending on river Charles boundness')
 Text(0.5, 1.0, 'Median value depending on river Charles boundness')
 
 sns.boxplot(y="MEDV", data=boston_df).set_title('Median value')
 Text(0.5, 1.0, 'Median value')
 
-Explanation The median value is slightlz above 20
 
 sns.catplot(x="CHAS", kind='count', data=boston_df)
 <seaborn.axisgrid.FacetGrid at 0x7f14a4372a10>
@@ -69,11 +63,6 @@ Ttest_indResult(statistic=-3.113291312794837, pvalue=0.003567170098137517)
 Conclusion
 The p-Value is much smaller than  α=0.05 . Hence we reject the hypothesis and conclude that there is a significant difference in the median values of houses depending on being bound to Charles river and not
 
-Is there a difference in Median values of houses (MEDV) for each proportion of owner occupied units built prior to 1940 (AGE)?
-Hypothesis
-Null Hypothesis There is no difference in median values for each proportion of owner occupied units built prior to 1940
-
-Alternate Hypothesis There is a difference in median values for each proportion of owner occupied units built prior to 1940
 
 ANOVA
 sns.boxplot(x="AGE_group", y="MEDV", data=boston_df).set_title('Median values for different age groups')
@@ -89,14 +78,6 @@ LeveneResult(statistic=2.7806200293748304, pvalue=0.06295337343259205)
 f_statistic, p_value = stats.f_oneway(younger_thirtyfive, thirtyfive_to_seventy, older_seventy)
 print("F_Statistic: {0}, P-Value: {1}".format(f_statistic,p_value))
 F_Statistic: 36.40764999196599, P-Value: 1.7105011022702984e-15
-Conclusion
-Since the p-value is smaller than  α=0.05 , we will reject the null hypothesis
-
-Can we conclude that there is no relationship between Nitric oxide concentrations and proportion of non-retail business acres per town?
-Hypothesis
-Null Hypothesis There is no relationship between Nitric oxide concentrations and proportion of non-retail business acres per town
-
-Alternate Hypothesis There is a relationship between Nitric oxide concentrations and proportion of non-retail business acres per town
 
 sns.scatterplot(x='NOX', y='INDUS', data=boston_df).set_title('Nitric oxide concentrations vs non-retail business acree per town')
 Text(0.5, 1.0, 'Nitric oxide concentrations vs non-retail business acree per town')
@@ -107,11 +88,6 @@ stats.pearsonr(boston_df['NOX'], boston_df['INDUS'])
 Conclusion
 Since the p-value is smaller than  α=0.05 , we reject the null hypothesis.
 
-What is the impact of an additional weighted distance to the five Boston employment centres on the median value of owner occupied homes?
-Hypothesis
-Null Hypothesis An additional weighted distance to the five Boston employment centres has no impact on the median value of owner occupied homes
-
-Alternate Hypothesis An additional weighted distance to the five Boston employment centres has an impact on the median value of owner occupied homes
 
 sns.scatterplot(x="DIS", y="MEDV", data=boston_df).set_title('Distance to employment centres vs Median values')
 Text(0.5, 1.0, 'Distance to employment centres vs Median values')
@@ -148,7 +124,3 @@ Skew:	1.466	Prob(JB):	5.59e-67
 Kurtosis:	5.424	Cond. No.	9.32
 
 
-Warnings:
-[1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
-Conclusion
-Since p < 0.05 there is evidence of correlation between the weighted distance to the five Boston employment centres and the median value of owner occupied homes.  R2=0.62  and the coefficient is 0.79.
